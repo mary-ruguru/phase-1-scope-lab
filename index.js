b00ca9623c24e0f76dd4e7bbec98b15d8fa91764
Maryann Ruguru Njuguna <maryannruguru20@gmail.com>
2022-11-26 20:51:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+   customerName = customerName.toUpperCase()
+    return customerName;
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = "";
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'test'
+}
+
+
+
+
+
+
+//describe('Fix the Scope', function() {
+ //   describe('declare customerName to be bob in global scope', function() {
+ //     it('returns the customerName', function() {
+ //       expect(window.customerName).to.equal('bob');
+ //     });
+ //   });
+  
+ //   describe('upperCaseCustomerName()', function() {
+ //     it('modifies the customerName variable', function() {
+ //       expect(window.customerName).to.equal('bob');
+  
+ //       upperCaseCustomerName();
+  
+ //       expect(window.customerName).to.equal('BOB');
+ //     });
+//    });
